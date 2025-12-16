1b76996609e9adbd206094cb69d553105fbe47bc
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-15 20:59:08 -0500

    Time: 327 ms (38.20%) | Memory: 18.5 MB (59.81%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -1,20 +1,19 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         mod = 10 ** 9 + 7
-        dp = [[-1] * (target+1) for _ in range(n+1)]
-        def f(dice, total):
-            if total > target:
-                return 0
-            if dice == n:
-                 return 1 if total == target else 0
-            if dp[dice][total] != -1:
-                return dp[dice][total]
-            ways = 0
-            for face in range(1, k+1):
-                ways += f(dice + 1, total + face)
-            dp[dice][total] = ways % mod
-            return dp[dice][total]
-        return f(0, 0)
+        dp = [[0] * (target+1) for _ in range(n+1)]
+        dp[n][target] = 1
+        for dice in range(n-1, -1, -1):
+            for total in range(target, -1 , -1):
+                ways = 0
+                for face in range(1, k+1):
+                    ways += (dp[dice + 1][total + face]) if total + face <= target else 0
+                dp[dice][total] = ways % mod
+        return dp[0][0]
+
+
+
+
 
 
             
