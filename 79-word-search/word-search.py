a236eb4746b7f955f743488dea1ccead636f6814
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-19 16:55:25 -0500

    Time: 2996 ms (88.53%) | Memory: 17.4 MB (99.65%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -3,25 +3,26 @@ class Solution:
         m, n = len(board), len(board[0])
         vis = [[0] * n for _ in range(m)]
         dir = {(-1, 0), (0, 1), (1, 0), (0, -1)}
-        self.found = False
         def findWord(i, j, idx):
             if idx == len(word):
-                self.found = True
-                return
+                return True
             for di, dj in dir:
                 ni, nj = i + di, j + dj
                 if 0 <= ni < m and 0 <= nj < n and board[ni][nj] == word[idx] and vis[ni][nj] == 0:
                     vis[ni][nj] = 1
-                    findWord(ni, nj, idx + 1)
+                    if findWord(ni, nj, idx + 1):
+                        return True
                     vis[ni][nj] = 0
+            return False
 
         for r in range(m):
             for c in range(n):
-                if board[r][c] == word[0] and self.found == False:
+                if board[r][c] == word[0]:
                     vis[r][c] = 1
-                    findWord(r, c, 1)
+                    if findWord(r, c, 1):
+                        return True
                     vis[r][c] = 0
-        return self.found
+        return False
 
 
 
