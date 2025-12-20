7621e1df6e83faf339b0a131fcf80b40647358b4
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-20 18:18:02 -0500

    Time: 11 ms (69.88%) | Memory: 17.8 MB (98.99%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,27 +1,25 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [["."] * n for _ in range(n)]
+        top_left, top_right, top = set(), set(), set()
         result = []
-        def getWays(idx, board, top_left, top_right, top):
+        def getWays(idx):
             if idx == n:
                 result.append(["".join(row) for row in board])
                 return
             for col in range(n):
-                valid = True
-                if idx - col in top_left: valid = False
-                if idx + col in top_right: valid = False
-                if col in top: valid = False
-                if valid:
-                    board[idx][col] = "Q"
-                    top_left.add(idx - col)
-                    top_right.add(idx + col)
-                    top.add(col)
-                    getWays(idx + 1, board, top_left, top_right, top)
-                    top_left.remove(idx - col)
-                    top_right.remove(idx + col)
-                    top.remove(col)
-                    board[idx][col] = "."
-        board = [["."] * n for _ in range(n)]
-        getWays(0, board, set(), set(), set())
+                if idx - col in top_left or idx + col in top_right or col in top:
+                    continue
+                board[idx][col] = "Q"
+                top_left.add(idx - col)
+                top_right.add(idx + col)
+                top.add(col)
+                getWays(idx + 1)
+                top_left.remove(idx - col)
+                top_right.remove(idx + col)
+                top.remove(col)
+                board[idx][col] = "."
+        getWays(0)
         return result
                     
 
