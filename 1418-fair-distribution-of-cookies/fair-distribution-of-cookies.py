a112407a440f4454b89403e9adaadc717528c3d7
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-22 15:57:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (99.50%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,8 +1,9 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         n = len(cookies)
+        # cookies.sort(reverse = True)
         arr = [0] * k
-        self.min_diff = float("inf")
+        self.min_diff = sum(cookies)
         def findUnfair(idx):
             if max(arr) >= self.min_diff:
                 return
@@ -10,9 +11,13 @@ class Solution:
                 self.min_diff = min(self.min_diff, max(arr))
                 return
             for i in range(k):
+                if arr[i] + cookies[idx] >= self.min_diff:
+                    continue
                 arr[i] += cookies[idx]
                 findUnfair(idx+1)
                 arr[i] -= cookies[idx]
+                if arr[i] == 0:
+                    break
         findUnfair(0)
         return self.min_diff
 
