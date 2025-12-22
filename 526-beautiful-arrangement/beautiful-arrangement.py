2ad5e707bcdd677c8d8782c02b030ad2577428c8
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-21 22:04:52 -0500

    Time: 52 ms (90.46%) | Memory: 21.1 MB (13.77%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,15 +1,16 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        used = [0] * (n+1)
-        def countWays(idx):
+        dp = {}
+        def countWays(idx, mask):
             if idx == n+1:
                 return 1
+            if (idx, mask) in dp:
+                return dp[(idx, mask)]
             cnt = 0
             for num in range(1, n+1):
-                if (num % idx == 0 or idx % num == 0) and used[num] == 0:
-                    used[num] = 1
-                    cnt += countWays(idx + 1)
-                    used[num] = 0
-            return cnt
-        return countWays(1)
+                if (num % idx == 0 or idx % num == 0) and (mask & (1 << num)) == 0:
+                    cnt += countWays(idx + 1, mask | (1 << num))
+            dp[(idx, mask)] = cnt
+            return dp[(idx, mask)]
+        return countWays(1, 0)
         
