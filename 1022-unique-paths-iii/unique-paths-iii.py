53bc9971493429cc7103e1678554ee0792329b0f
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-23 18:35:16 -0500

    Time: 9 ms (78.64%) | Memory: 17.2 MB (98.66%) - LeetSync

---
+++ 1022-unique-paths-iii/unique-paths-iii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        m, n = len(grid), len(grid[0])
+        vis = [[0] * n for _ in range(m)]
+        def countPaths(i, j, unused):
+            if grid[i][j] == 2:
+                if unused == 0:
+                    return 1
+                return 0
+            ways = 0
+            for di, dj in dir:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < m and 0 <= nj < n and grid[ni][nj] != -1 and vis[ni][nj] == 0:
+                    unused -= 1
+                    vis[ni][nj] = 1
+                    ways += countPaths(ni, nj, unused)
+                    unused += 1
+                    vis[ni][nj] = 0
+            return ways
+
+        unused = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    start_r, start_c = i, j
+                elif grid[i][j] in (0, 2):
+                    unused += 1
+        vis[start_r][start_c] = 1
+        return countPaths(start_r, start_c, unused)
+        
+
+            
+        
