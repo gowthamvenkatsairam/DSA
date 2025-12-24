7210d24f7517de79ed49f195c9be88bdb81a82ce
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-23 20:47:01 -0500

    Time: 508 ms (37.16%) | Memory: 17.5 MB (96.17%) - LeetSync

--- 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.py
+++ 2078-maximum-compatibility-score-sum/maximum-compatibility-score-sum.py
@@ -4,21 +4,26 @@ class Solution:
         self.ans = float("-inf")
         used_mentors = [0] * m
         self.score = 0
+        computed = [[0] * m for _ in range(m)]
+        for i in range(m):
+            for j in range(m):
+                for k in range(n):
+                    if students[i][k] == mentors[j][k]:
+                        computed[i][j] += 1
+                        
         def solve(idx):
             if idx == m:
                 self.ans = max(self.ans, self.score)
                 return
             for i in range(m):
-                same = 0
-                if used_mentors[i] == 1: continue
-                for k in range(n):
-                    if students[idx][k] == mentors[i][k]:
-                        same += 1
+                if used_mentors[i] == 1:
+                    continue
                 used_mentors[i] = 1
-                self.score += same
+                self.score += computed[idx][i]
                 solve(idx+1)
                 used_mentors[i] = 0
-                self.score -= same
+                self.score -= computed[idx][i]
+        
         solve(0)
         return self.ans
                 
