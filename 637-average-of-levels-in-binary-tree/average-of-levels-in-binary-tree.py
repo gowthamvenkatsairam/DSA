5051fd9ded4ad395faaa989156f024389149a5a3
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-26 23:26:54 -0500

    Time: 4 ms (23.92%) | Memory: 19 MB (93.53%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -9,14 +9,14 @@ class Solution:
         dq = deque([root])
         result = []
         while dq:
-            total, cnt = 0, 0
-            for _ in range(len(dq)):
+            total = 0
+            lvl_cnt = len(dq)
+            for _ in range(lvl_cnt):
                 node = dq.popleft()
                 total += node.val
-                cnt += 1
                 if node.left: dq.append(node.left)
                 if node.right: dq.append(node.right)
-            result.append(total/cnt)
+            result.append(total/lvl_cnt)
         return result
 
         
