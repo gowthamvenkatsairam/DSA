a0001052766b7b4030771e55269b6b62ebbb11d9
gowthamvenkatsairam <57246314+gowthamvenkatsairam@users.noreply.github.com>
2025-12-29 16:00:06 -0500

    Time: 472 ms (74.32%) | Memory: 106.4 MB (62.30%) - LeetSync

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -4,19 +4,18 @@ class Solution:
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        vis = [0] * n
         ans = [0] * n
-        def dfs(node):
-            vis[node] = 1
+        def dfs(node, parent):
             freq = [0] * 26
             for child in graph[node]:
-                if vis[child] != 1:
-                    subtree_freq = dfs(child)
-                    for i in range(26):
-                        freq[i] += subtree_freq[i]
+                if child == parent:
+                    continue
+                subtree_freq = dfs(child, node)
+                for i in range(26):
+                    freq[i] += subtree_freq[i]
             freq[ord(labels[node]) - ord("a")] += 1
             ans[node] = freq[ord(labels[node]) - ord("a")]
             return freq
-        dfs(0)
+        dfs(0, -1)
         return ans
         
